6e956832fb0015ec2113f3cc336336cef5cc0a3b
Joseph <josewsq101@gmail.com>
2022-03-23 01:06:23 -0300

    iniciado repo

---
+++ AgendaMVC/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using AgendaMVC.Models.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AgendaMVC.Models;
+
+
+namespace AgendaMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<Registers> lst = new List<Registers>();
+            using (GitAgendaEntities db = new GitAgendaEntities())
+            {
+               
+            }
+                return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
