31c9d315f824af107d67ccecc637e36d0d448fb2
Joseph <josewsq101@gmail.com>
2022-03-23 11:32:21 -0300

    agregando model tipo helper

---
+++ AgendaMVC/Models/View/helperModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AgendaMVC.Models.View
+{
+    public class helperModel
+    {
+        [Required]
+        [StringLength (100, ErrorMessage = " el {0} tiene que tener minimo {1} caracter", MinimumLength = 1)]
+        [Display(Name = "Nombre")]
+        public string nombre { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = " el {0} tiene que tener minimo {1} caracter", MinimumLength = 1)]
+        [Display(Name = "Apellido")]
+        public string apellido { get; set; }
+    }
+}
